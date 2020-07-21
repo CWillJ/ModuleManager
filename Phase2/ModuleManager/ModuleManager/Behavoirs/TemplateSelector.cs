20742598d8172d6494a22ef0b9798efd1c33dc8e
cwillj21 <wjohnson@pva.net>
2020-07-21 16:00:45 -0400

    DataTemplate

---
+++ Phase2/ModuleManager/ModuleManager/Behavoirs/TemplateSelector.cs
@@ -0,0 +1,36 @@
+﻿namespace ModuleManager.Behavoirs
+{
+    using System.Windows;
+    using System.Windows.Controls;
+    using ModuleManager.Classes;
+
+    /// <summary>
+    /// Used to selected a DataTemplate in the view.
+    /// </summary>
+    public class TemplateSelector : DataTemplateSelector
+    {
+        /// <summary>
+        /// Used to select the DataTemplate used in the view.
+        /// </summary>
+        /// <param name="item">Object.</param>
+        /// <param name="container">Container.</param>
+        /// <returns>A DataTemplate depending on the object passed in.</returns>
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            if (container is FrameworkElement element && container != null && item != null)
+            {
+                if (item is Module)
+                {
+                    return element.FindResource("moduleTemplate") as DataTemplate;
+                }
+
+                if (item is ModuleMember)
+                {
+                    return element.FindResource("moduleMemberTemplate") as DataTemplate;
+                }
+            }
+
+            return null;
+        }
+    }
+}
